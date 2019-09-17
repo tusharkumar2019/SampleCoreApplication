dd201192af0ac276b780ddba393fd99a142b0678
Tushar Kumar Patra <tusarkumarp@esntechnologies.co.in>
2019-09-17 13:32:51 +0530

    2nd commit

--- SampleCoreWebApplication/Startup.cs
+++ SampleCoreWebApplication/Startup.cs
@@ -18,7 +18,7 @@ namespace SampleCoreWebApplication
         {
             Configuration = configuration;
         }
-        //Comment
+      
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
